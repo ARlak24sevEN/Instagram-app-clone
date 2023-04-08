2ed192350433de9986567c99bebbb474daf6349a
Arlak <arlak.a@orcsoft.co.th>
2023-04-08 14:45:06 +0700

    Initial Commit

---
+++ Instagram-app-clone/Instagram_app_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_app_cloneApp.swift
+//  Instagram-app-clone
+//
+//  Created by Arlak Abdulloh on 8/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_app_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
