eda0261fc3e0a70780b7402a3af9dc93aba3fada
Arlak <arlak.a@orcsoft.co.th>
2023-04-10 11:40:10 +0700

    first commit

---
+++ Instagram-app-clone/HomeInstagramView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeInstagram.swift
+//  Instagram-app-clone
+//
+//  Created by Arlak Abdulloh on 8/4/2566 BE.
+//
+
+import SwiftUI
+
+struct HomeInstagram: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeInstagram_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeInstagram()
+    }
+}
